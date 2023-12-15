3041bf8a749d5052de29835b0e4729409937d4b4
Alex Boyle <alex.boyle@sony.com>
2023-12-15 12:47:18 -0800

    Adding javascript test

---
+++ customize_jenkins.js
@@ -0,0 +1,8 @@
+// Define the new image URL
+var newImageUrl = "https://www.jenkins.io/images/logos/santa-claus/santa-claus.svg";
+
+// Find the image element by id
+var imageElement = document.getElementById("jenkins-head-icon");
+
+// Change the image source
+imageElement.src = newImageUrl;
